8fc4a30372eb148da2cf18348cd903a347b8b7c2
Shashank Shekhar <118442424+shashank-zopsmart@users.noreply.github.com>
2024-10-18 13:36:33 +0530

    Add docker cmd

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"gofr.dev/pkg/gofr"
+	"os/exec"
 )
 
 func main() {
 	app := gofr.New()
 
 	//app.EnableOAuth("http://localhost:9001/jwks", 5)
+
+	op, err := exec.Command("docker", "ps").CombinedOutput()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(op))
 
 	app.GET("/", func(c *gofr.Context) (interface{}, error) {
 		return "ok", nil
